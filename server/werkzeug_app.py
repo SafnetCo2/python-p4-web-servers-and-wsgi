2398849094ea516e320a58baa6c14911e8c93328
Jospine <nzi19001@byui.edu>
2024-05-21 23:32:31 +0300

    done

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-from werkzeug.wrappers import Request, Response
+from werkzeug.wrappers import Request,Response
 
 @Request.application
 def application(request):
-    print(f'This web server is running at {request.remote_addr}')
-    return Response('A WSGI generated this response!')
-
-if __name__ == '__main__':
+    print(f'server running at {request.remote_addr}')
+    return Response('A werkzeung generated request')
+if __name__=='__main__':
     from werkzeug.serving import run_simple
     run_simple(
         hostname='localhost',
